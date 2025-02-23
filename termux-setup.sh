6d6b72508f45027820623a1e57ee4f2638766779
Myon <suzkin52655@gmail.com>
2025-02-23 17:20:43 +0900

    Add Ubuntu installation

--- termux-setup.sh
+++ termux-setup.sh
@@ -8,8 +8,7 @@ pkg upgrade
 
 read -p "Do you want to install recommended packages? (y/n): " choice
 
-if [ "$choice" = "y" ]; then
-  # commands
+if [ "$choice" = "y" ]; then                      # commands
   echo "Installing... please wait"
   pkg install wget git vim proot
   echo "Please grant storage access"
@@ -26,5 +25,19 @@ pkg install openssh iproute2
 echo "SSH installed."
 else
 echo "SSH not installed."
+fi
+read -p "Do you want to install Ubuntu?(y/n): " ubuntuchoice
+if [ "$ubuntuchoice" = "y" ]; then
+echo "Installing Ubuntu... please wait"
+cd ~
+git clone https://github.com/MFDGaming/ubuntu-in-termux.git
+cd ubuntu-in-termux
+chmod +x ubuntu.sh
+./ubuntu.sh -y
+echo "Ubuntu installed."
+echo "cd ubuntu-in-termux and"
+echo "./startubuntu.sh to start Ubuntu."
+else
+echo "Ubuntu not installed."
 fi
 echo "All set!"
